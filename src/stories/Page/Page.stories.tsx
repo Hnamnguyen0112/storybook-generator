b133097d45701d2c58cfab0651e26d8f4ec24a2e
Nam Nguyen <alexnguyenth96@gmail.com>
2022-01-23 23:07:34 +0700

    split folder

--- src/stories/Page/Page.stories.tsx
+++ src/stories/Page/Page.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Page } from './Page';
-import * as HeaderStories from './Header.stories';
+import { Page } from './index';
+import * as HeaderStories from '../Header/Header.stories';
 
 export default {
   title: 'Example/Page',
